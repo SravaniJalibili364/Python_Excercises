fa9b55b2d0a5cec90b5318ae5113343e30ed7dd7
SravaniJalibili364 <89896513+SravaniJalibili364@users.noreply.github.com>
2021-09-16 13:56:16 +0530

    Update list_first_last_ele.py

--- Day1/Python_Basics/list_first_last_ele.py
+++ Day1/Python_Basics/list_first_last_ele.py
@@ -1,3 +1,4 @@
 #Split the following string into a list using the separator and print the first, last elements
-str_input = input().split("-")          # Taking the input as string with hypen separated and using split function it converting into list
+
+str_input = input().split("-")                        # Taking the input as string with hypen separated and using split function it converting into list
 print(str_input[0],str_input[-1])                     # It will print the firstelement and lastelement of the list
