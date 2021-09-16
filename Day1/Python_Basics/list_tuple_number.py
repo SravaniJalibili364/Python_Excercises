904377075da4c101f1c496b171bd126e1c983dff
SravaniJalibili364 <89896513+SravaniJalibili364@users.noreply.github.com>
2021-09-16 13:33:41 +0530

    Update list_tuple_number.py

--- Day1/Python_Basics/list_tuple_number.py
+++ Day1/Python_Basics/list_tuple_number.py
@@ -1,6 +1,10 @@
-Numbers = input()
-Numbers_list = Numbers.split(",")
-print("List:",Numbers_list)
-Tuple_List = tuple(Numbers_list)
-#print the statement
-print("Tuple",Tuple_List)
+#Prompt user to enter a sequence of comma-separated numbers and generate a list and a tuple with those numbers
+
+Num_input = input()                                     # Taking input as sequence of numbers with comma-separated
+Numbers_list = list(map(int,Num_input.split(",")))      # Using split function sequence of numbers converted into list and then each number
+                                                        # converted into int type from string type
+
+print("List:",Numbers_list)                             # Now it will print the numbers list
+Tuple_List = tuple(Numbers_list)                        # List converting into tuple 
+
+print("Tuple",Tuple_List)                               # Now tuple will print with numbers as an tuple items
