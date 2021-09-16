1666ad0bb114fddfb8e18fc1405b453c0ea36332
SravaniJalibili364 <89896513+SravaniJalibili364@users.noreply.github.com>
2021-09-16 13:18:21 +0530

    Update List_Reverse.py

--- Day1/Python_Basics/List_Reverse.py
+++ Day1/Python_Basics/List_Reverse.py
@@ -1,17 +1,17 @@
 #Prompt user for first, last names and print them in reverse order.
 
 
-def reverse_strng_function(full_name):
-    reverse_string = ""
-    for i in Full_name:
-        reverse_string = i + reverse_string
+def reverse_strng_function(full_name):                     # Defining the function to reverse the string
+    reverse_string = ""                                    # creating the empty variable for to store the string
+    for i in Full_name:                                    # Iterating the string 
+        reverse_string = i + reverse_string                # it will every letter to the first
 
     return reverse_string
 
-first_name = input()
-second_name = input()
-Full_name = first_name + second_name
-#result = Full_name[::-1]      #this is the builtin function
-output_rev_string = reverse_strng_function(Full_name)
-print(output_rev_string)
+f_name = input()                                        #Taking first_name as input from the user
+l_name = input()                                       #Taking second_name as input from the user
+Full_name = f_name+ l_name
+#result = Full_name[::-1]                                   # this is the builtin method to print the reverse of the string
+output_rev_string = reverse_strng_function(Full_name)       # Calling function to reverse the string and it will return to output_rev_string variable
+print(output_rev_string)                                    # It will print the reverse of the string
 
