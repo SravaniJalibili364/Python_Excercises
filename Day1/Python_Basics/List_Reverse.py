ea9160135a3149f53c004e9ce6397910602504a9
SravaniJalibili364 <89896513+SravaniJalibili364@users.noreply.github.com>
2021-09-16 13:08:07 +0530

    Update List_Reverse.py

--- Day1/Python_Basics/List_Reverse.py
+++ Day1/Python_Basics/List_Reverse.py
@@ -1,5 +1,17 @@
+#Prompt user for first, last names and print them in reverse order.
+
+
+def reverse_strng_function(full_name):
+    reverse_string = ""
+    for i in Full_name:
+        reverse_string = i + reverse_string
+
+    return reverse_string
+
 first_name = input()
 second_name = input()
 Full_name = first_name + second_name
-result = Full_name[::-1]
-print(result)
+#result = Full_name[::-1]      #this is the builtin function
+output_rev_string = reverse_strng_function(Full_name)
+print(output_rev_string)
+
