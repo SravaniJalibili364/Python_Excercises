5a4b841acc397df6f7140f2c3b1034c3383026c8
SravaniJalibili364 <89896513+SravaniJalibili364@users.noreply.github.com>
2021-10-12 10:22:38 +0530

    Update current_date_time.py

--- Day2/current_date_time.py
+++ Day2/current_date_time.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 # datetime object containing current date and time
 Current_date_time = datetime.today()
- 
+print(Current_date_time)
 
 date_Time = Current_date_time.strftime("%d-%m-%Y %H:%M:%S")
 
